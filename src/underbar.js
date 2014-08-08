374a0eca0060ef79f1e7507f29ee18f95fb37a9c
Greg Ferrell <gregerg@gmail.com>
2014-08-08 14:20:01 -0500

    Added functionality for _.map()

--- src/underbar.js
+++ src/underbar.js
@@ -123,6 +123,11 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var output = [];
+    collection.forEach(function(value, index, collection) {
+      output.push(iterator(value,index,collection));
+    });
+    return output;
   };
 
   /*
